166640c0c6be9f961b6eb1eef607ec8710a2fe05
Ankita Mishra <ankitamishra@Ankitas-MacBook-Air.local>
2021-12-07 21:16:17 -0500

    added text input

--- App.js
+++ App.js
@@ -6,97 +6,45 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section >
-            Hello World!! This is a test.
-          </Section>
-          
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+ import React, { useState } from 'react'
+ import {
+   StyleSheet,
+   SafeAreaView,
+   Text,
+   TextInput,
+ } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+ 
+ const App = () => {
+  const [text, setText] = useState('');
+     return (
+       <SafeAreaView style={{backgroundColor: '#FFFFFF'}}>
+         <TextInput
+          style={styles.input}
+          placeholder="Search for a book..."
+          onChangeText={text => setText(text)}
+          defaultValue={text}
+         />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {text.split(' ').map((word) => word).join(' ')}
+        </Text>
+       </SafeAreaView>
+     )
+ }
+ 
+ const styles = StyleSheet.create({
+  input: {
+    backgroundColor: Colors.black,
+    paddingTop: 40,
+    paddingLeft: 20,
+    paddingBottom: 40,
+    height: 40,
+    width: 350,
+    marginTop: 50,
+    marginLeft: 20,
+    borderWidth: 1,
+    fontSize:20,
   },
 });
-
-export default App;
+ 
+ export default App;
